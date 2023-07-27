539473b28be03c8ad3e69ad1d8496d0ecd2a4e19
Jsheen98 <jsheendev@gmail.com>
2023-07-27 14:39:50 -0600

    Added several basic rendering elements, set up db

---
+++ app/Models/Listing.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+class Listing
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+            ]
+        ];
+    }
+
+    public static function find($id) {
+        $listings = self::all();
+
+        foreach($listings as $listing) {
+            if($listing['id'] == $id) {
+                return $listing;
+            }
+        }
+    }
+}
