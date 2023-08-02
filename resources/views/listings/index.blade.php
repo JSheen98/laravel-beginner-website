4811225f52598528dd13b9bc0c7105bd62f53f28
Jsheen98 <jsheendev@gmail.com>
2023-08-02 14:28:47 -0600

    Added search and filter by tag functionality

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,8 +1,6 @@
-@extends('layout')
-
-@section('content')
-@include('partials._hero')
-@include('partials._search')
+<x-layout>
+    @include('partials._hero')
+    @include('partials._search')
 
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
@@ -16,4 +14,4 @@
 
     </div>
 
-@endsection
+</x-layout>
